ed5dbe4adbe93c7fb7295609906ec62add8f6758
lugh-tuatha <aceonsteam001@gmail.com>
2025-09-05 12:42:15 +0800

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Events } from './features/events/events';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'events',
+        component: Events
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink,
+    MainLayout
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
