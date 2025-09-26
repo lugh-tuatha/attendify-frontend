48f4cc254ea4963771e63b308c71c79f211e39bd
Ace <aceonsteam001@gmail.com>
2025-09-26 12:56:35 +0800

    improve: event-detail design, Registered attendee count implementation, search & pagination feature

--- src/app/features/events/pages/event-detail/event-detail.ts
+++ src/app/features/events/pages/event-detail/event-detail.ts
@@ -1,28 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { LucideAngularModule, CornerDownLeft, Search } from 'lucide-angular';
+
+import { LTHMIProfile } from '../../models/registered-attendee.model';
 import { EventModel } from '../../models/event.model';
 import { EventsService } from '../../services/events';
-import { CdkTableModule } from '@angular/cdk/table';
-import { MatTableDataSource } from '@angular/material/table';
-import { LTHMIProfile } from '../../models/registered-attendee.model';
-import { Button } from "../../../../shared/ui/button/button";
 
-const ELEMENT_DATA: any[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { Button } from "../../../../shared/ui/button/button";
+import { StatCard } from '../../../../shared/ui/stat-card/stat-card';
 
 @Component({
   selector: 'app-event-detail',
-  imports: [RouterLink, CdkTableModule, Button],
+  imports: [
+    RouterLink, 
+    MatTableModule, 
+    MatPaginatorModule, 
+    Button, 
+    StatCard, 
+    CdkTableModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    LucideAngularModule
+  ],
   templateUrl: './event-detail.html',
   styleUrl: './event-detail.css'
 })
@@ -31,12 +37,17 @@ export class EventDetail {
   private route = inject(ActivatedRoute);
   private eventsService = inject(EventsService);
 
+  readonly CornerDownLeft = CornerDownLeft;
+  readonly Search = Search;
+
   event: EventModel | null = null;
   isEventLoading = false;
 
   registeredAttendeesColumns: string[] = ['fullName', 'primaryLeader', 'churchHierarchy','memberStatus', 'actions'];
   dataSource = new MatTableDataSource<LTHMIProfile>([]);
   isRegisteredAttendeesLoading = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit(): void {
     const eventId = this.route.snapshot.paramMap.get('id');
@@ -45,6 +56,10 @@ export class EventDetail {
       this.loadEvent(eventId);
       this.loadRegisteredAttendees(eventId);
     };
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   private loadEvent(id: string): void {
@@ -69,12 +84,21 @@ export class EventDetail {
     this.eventsService.getAllRegisteredAttendees(id).subscribe({
       next: (response) => {
         this.dataSource.data = response.data;
-        console.log(this.dataSource.data)
+        this.isRegisteredAttendeesLoading = false;
       },
       error: (error) => {
         console.error('Error loading registered attendees:', error);
         this.isRegisteredAttendeesLoading = false;
       }
     })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
--- src/app/shared/ui/stat-card/stat-card.ts
+++ src/app/shared/ui/stat-card/stat-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stat-card',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './stat-card.html',
   styleUrl: './stat-card.css'
 })
 export class StatCard {
-
+  @Input() title!: string;
+  @Input() value!: number;
+  @Input() icon!: string;
+  @Input() description!: string;
 }
